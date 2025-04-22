868de049daae8c08a8df1ad18355833c9092f670
OppositeWolf770 <dhoelzeman@proton.me>
2025-04-21 23:57:07 -0500

    first commit

---
+++ SoulTarots.lua
@@ -0,0 +1,39 @@
+AltTexture {
+    key = 'tarots',
+    set = 'Tarot',
+    path = 'Tarots.png',
+    keys = {
+        'c_fool',
+        'c_magician',
+        'c_high_priestess',
+        'c_empress',
+        'c_emperor',
+        'c_heirophant',
+        'c_lovers',
+        'c_chariot',
+        'c_justice',
+        'c_hermit',
+        'c_wheel_of_fortune',
+
+    },
+    soul_keys = {
+        'c_fool',
+        'c_magician',
+        'c_high_priestess',
+        'c_empress',
+        'c_emperor',
+        'c_heirophant',
+        'c_lovers',
+        'c_chariot',
+        'c_justice',
+        'c_hermit',
+        'c_wheel_of_fortune',
+    },
+}
+
+TexturePack {
+    key = 'soul_tarots',
+    textures = {
+        'soul_tarots_tarots'
+    },
+}
