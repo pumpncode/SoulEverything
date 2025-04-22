d883f57af7577d7fc8658b617d4c2d08d7197de3
OppositeWolf770 <dhoelzeman@proton.me>
2025-04-22 00:08:52 -0500

    Added localization for the texture pack in the Malverk manager menu

---
+++ localization/default.lua
@@ -0,0 +1,13 @@
+return {
+    descriptions = {
+        texture_packs = {
+            texpack_soul_tarots_soul_tarots = {
+                name = 'SoulTarots',
+                text = {
+                    'Makes the figures in the Tarot Cards',
+                    'pop out of the card',
+                }
+            }
+        }
+    }
+}
